420893d80de2e0a9a2447f18c91ee1a336570b68
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-06-19 09:46:29 +0000

    Template

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/template', name: 'app_home_template')]
+    public function template()
+    {
+        return $this->render('home/template.html.twig');
+    }
+}
