4ce3cd0c9b7452c148e8807ba10aa8826db0efa5
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-07-08 06:26:23 +0000

    Sync Data

--- src/Service/Synchronisation.php
+++ src/Service/Synchronisation.php
@@ -85,8 +85,7 @@ class Synchronisation
 
         // Mise a jour de la table produit
         foreach ($facture['produits'] as $produit){
-//            $entity = $this->produitRepository->findOneBy(['reference' => (int) $produit['code']]); //dd($entity);
-            $entity = $this->produitRepository->findByCodeOrReference($produit['code']); dd($entity);
+            $entity = $this->produitRepository->findOneBy(['reference' => (int) $produit['code']]);
             if (!$entity) return;
 
             $entity->setStock((int) $entity->getStock() - (int)$produit['quantite']);
