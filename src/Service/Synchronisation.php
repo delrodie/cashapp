c119ab7b864e11a4d0ba87b194a8f694abac1ef6
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-07-08 04:56:39 +0000

    Sync boucle produit

--- src/Service/Synchronisation.php
+++ src/Service/Synchronisation.php
@@ -85,7 +85,7 @@ class Synchronisation
 
         // Mise a jour de la table produit
         foreach ($facture['produits'] as $produit){
-            $entity = $this->produitRepository->findOneBy(['reference' => $produit['code']]); dd($produit['code']);
+            $entity = $this->produitRepository->findOneBy(['reference' => (int) $produit['code']]); dd($entity);
             if (!$entity) return;
 
             $entity->setStock((int) $entity->getStock() - (int)$produit['quantite']);
