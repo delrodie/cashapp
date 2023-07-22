e8111eccce8b48c39db7e95a51e028e7c1d5ad2b
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-07-22 20:30:16 +0000

    syncSynchro 5

--- src/Service/Synchronisation.php
+++ src/Service/Synchronisation.php
@@ -225,7 +225,7 @@ class Synchronisation
     }
 
     public function synchro(mixed $synchroData): int
-    { dd($synchroData);
+    {
         $exist = $this->synchroRepository->findOneBy(['code'=>$synchroData['code']]);
         if ($exist) return 2;
 
@@ -237,6 +237,7 @@ class Synchronisation
         $newSynchro->setCreatedAt(new \DateTime($synchroData['createdAt']['date']));
         $newSynchro->setSync(true);
         $newSynchro->setContent($synchroData['content']);
+        dd($newSynchro);
 
         switch ($synchroData['action']){
             case 'SUPPRESSION':
