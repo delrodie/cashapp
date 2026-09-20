26cfbdcbb495bb347fd7affc2f68018c65e2199d
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2026-09-20 13:56:50 +0000

    hotfix(url): correction de l'url de details benefice

--- src/Controller/Main/DetailsBeneficeController.php
+++ src/Controller/Main/DetailsBeneficeController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/details-benefice')]
 class DetailsBeneficeController extends AbstractController
 {
     public function __construct(private readonly FactureRepository $factureRepository)
